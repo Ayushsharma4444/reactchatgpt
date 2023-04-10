420365fd391d44bdeeac914601c9ec62253754f9
Ayushsharma4444 <ayushsharma81852.com>
2023-04-10 17:56:58 +0530

    new api

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 // const API_KEY = "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";
-const API_KEY = "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";
+const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
