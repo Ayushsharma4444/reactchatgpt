59c2a64ccfd1db5ba5ce24ae00217a46e805c005
Ayushsharma4444 <ayushsharma81852.com>
2023-04-10 17:29:51 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 // const API_KEY = "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";
-const API_KEY = "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";
+const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
@@ -64,6 +64,7 @@ function App() {
       ]
     }
 
+    // await fetch("https://api.openai.com/v1/models", 
     await fetch("https://api.openai.com/v1/chat/completions", 
     {
       method: "POST",
